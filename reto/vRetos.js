a0a037dca325a59f44c89ed8cd4eee2b89bb128e
Nightshadow7 <hernandezdiazanthonilexandre@gmail.com>
2023-05-05 14:44:45 -0500

    guardando avance

--- reto/vRetos.js
+++ reto/vRetos.js
@@ -1,13 +1,26 @@
-
 import * as calza from './Calzado.js'
 import * as prenda from './Prenda.js'
 
 
+let tipoPrenda = document.querySelector('.seleccionPrenda').style.display = 'none';
+let tallaPrenda = document.querySelector('.tallaPrenda').style.display = 'none';
+let planchar = document.querySelector('.planchar').style.display = 'none';
+let seleccionCalzado = document.querySelector('.seleccionCalzado').style.display = 'none';
+let tallaCalzado = document.querySelector('.tallaCalzado').style.display = 'none';
 
-let prenda1 = document.querySelector('#aaa')
 
 
+let tipoProducto = function () {
+    let prenda = document.querySelector('.prenda').value;
+    let calzado = document.querySelector('.calzado').value;
 
-
-
+    if (calzado.addEventListener('change', tipoProducto)) {
+        seleccionCalzado = document.querySelector('.seleccionCalzado').style.display = 'block';
+        tallaCalzado = document.querySelector('.tallaCalzado').style.display = 'block';
+    } else if (prenda.addEventListener('change', tipoProducto)) {
+        tipoPrenda = document.querySelector('.seleccionPrenda').style.display = 'block';
+        tallaPrenda = document.querySelector('.tallaPrenda').style.display = 'block';
+        planchar = document.querySelector('.planchar').style.display = 'block';
+    }
+}
 
