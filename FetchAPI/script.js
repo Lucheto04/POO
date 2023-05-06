00bf1ac8a0d45617f21ba0e8b56eb1f96ccb0628
Lucheto04 <124000317+Lucheto04@users.noreply.github.com>
2023-05-06 17:00:46 -0500

    terminando la pelea pokemon

--- FetchAPI/script.js
+++ FetchAPI/script.js
@@ -1,62 +1,37 @@
-
-
-
 let id1 = Math.round(Math.random() * 150);
 let id2 = Math.round(Math.random() * 150);
-
-
-console.log(id1);
-console.log(id2);
-
-
 function pelea() {
-
-
-
     // Primer Pokemon
     fetch(`https://pokeapi.co/api/v2/pokemon/${id1}`)
     .then(restult => restult.json())
     .then(data => {
-
-        let nombrePokemon = (data.name);
-        let widthPokemon = (data.width);
-        let heighPokemon = (data.heigh);
-    
+        let nombrePokemon = (data.name);    
         document.querySelector('#pokemon1').innerHTML = /*html*/ `
-        <img src="${data.sprites.front_default}" alt="" styles>
+        <img src="${data.sprites.front_default}" style="border: 1px solid black;" alt="">
         `;
-
         document.querySelector('#nombrePokemon1').innerHTML = /*html*/ `
         <h1>${nombrePokemon}</h1>
+        <p>${data.base_experience}</p>
         `;
-
-
     });
-
-
     // Segundo Pokemon
     fetch(`https://pokeapi.co/api/v2/pokemon/${id2}`)
     .then(restult => restult.json())
     .then(data => {
-
         let nombrePokemon = (data.name);
         let widthPokemon = (data.width);
         let heighPokemon = (data.heigh);
-    
         document.querySelector('#pokemon2').innerHTML = /*html*/ `
-        <img src="${data.sprites.front_default}" alt="">
+        <img src="${data.sprites.front_default}" style="border: 1px solid black;" alt="">
         `;
-
         document.querySelector('#nombrePokemon2').innerHTML = /*html*/ `
         <h1>${nombrePokemon}</h1>
+        <p>${data.base_experience}</p>
         `;
-
+        versus = document.querySelector('#versus').style.display = 'block';
     });
 }
-
-
-
-
+let versus = document.querySelector('#versus').style.display = 'none';
 window.addEventListener("load", function () {
     document.getElementById("pelea").addEventListener("click", pelea);
 });
